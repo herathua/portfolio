2b2fc5baf005d001e90e27ef2df7c8cbfbd6ed6b
Uvindu  Herath <110981456+herathua@users.noreply.github.com>
2024-11-23 14:18:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Replace 'your-repository-name' with your GitHub repository name
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/portfolio/', // Required for GitHub Pages
 });
